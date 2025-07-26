e4a06a3f25514dfc50fc523d055d8a1929ef9326
Shantanu Singh <distributedservices.shan@gmail.com>
2025-07-27 00:38:11 +0530

    Feat(Server): Added socket integration

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.use(express.json());
 
 // ✅ Fix CORS: allow actual headers not values
 app.use(cors({
-    origin: ORIGIN, // fallback to "*" for dev if undefined
+    origin: ORIGIN,
     methods: ['GET', 'POST'],
     allowedHeaders: ['Content-Type'], // correct header
     credentials: false
@@ -32,11 +32,35 @@ app.use(morgan('combined'));
 
 logger.info(`{module: server.js} inited all variables. Now working on main logic.`);
 
+const online_users = new Set();
+
+io.on('connection', socket => {
+
+    logger.info(`{module: server.js} [io.on connection] ${socket.id} connected`);
+
+    socket.on('user-landed', () => {
+        logger.info(`{module: server.js} [io.on connection] [new-user] ${socket.id} joined`);
+        online_users.add(socket.id);
+    })
+
+    socket.on('disconnect', () => {
+        logger.info(`{module: server.js} [io.on connection] ${socket.id} disconnected`);
+        online_users.delete(socket.id);
+    });
+
+})
+
 app.get('/', function(req, res) {
     logger.info(`{module: server.js} [app.get /] ENTRY`);
     res.send('Hello World!');
     logger.info(`{module: server.js} [app.get /] EXIT`);
 });
+
+app.get('/online-user', function(req, res){
+    logger.info(`{module: server.js} [app.get /online-user] ENTRY`);
+    res.send(`There are ${online_users.size} users online.`);
+    logger.info(`{module: server.js} [app.get /online-user] EXIT`);
+})
 
 server.listen(PORT, function(){
     logger.info('✅ THIS SERVICE IS FOR REAL-TIME TRACKING OF NUMBER OF USERS. Listening on port ' + PORT);
